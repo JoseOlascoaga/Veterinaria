59856f7de3b9b7a5c01c164a110631f84a6c0faf
JoseOlascoaga <olascoaga048@gmail.com>
2023-04-05 15:32:34 -0500

    Se agrega el proyecto

---
+++ src/app/paginas/sentimientosyemociones/sentimientosyemociones.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sentimientosyemociones',
+  templateUrl: './sentimientosyemociones.component.html',
+  styleUrls: ['./sentimientosyemociones.component.css']
+})
+export class SentimientosyemocionesComponent {
+
+}
